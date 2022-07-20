8a3c9f2c16ad8869c208f1539712be2f62be73e6
Chris Hyde <108821220+chr1ce@users.noreply.github.com>
2022-07-20 15:22:37 -0400

    Added T2AI script that determines negative emotion
    This checks a list of negative emotions against the list of matched emotions given by the AI. If any match, an example assist is shown. This would show multiple assists if multiple phrases matched, but the program ensures that no duplicates are shown, so simplicity is preferred.

---
+++ T2AI_neg_emotion.py
@@ -0,0 +1,8 @@
+relevant_cat = ["angry","sad","frustrated", "disgusted", "afraid"]
+categories = ["angry","sad","frustrated","appreciative","amused","content","disgusted","afraid","happy","relieved","surprised"]
+result = ai_categorize(msg, categories)
+
+if not is_before_ticket_submit:
+	for phrase in relevant_cat:
+		if phrase in result["matches"]:
+			tier2assist.append({'msg':'Would you like a cookie?','action':'https://www.youtube.com/watch?v=e1xCU9ydG-A'})
